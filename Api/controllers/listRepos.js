d6fd697f6394082e5a93365cff1193fe64676d4c
Francisco Chaves <francisco.jschaves@gmail.com>
2021-05-06 19:48:58 -0300

    refactor: Set default C# language

--- Api/controllers/listRepos.js
+++ Api/controllers/listRepos.js
@@ -5,21 +5,19 @@ module.exports = async (req, res) => {
   try {
     const { username } = req.params;
 
-    const language = req.query.language;
+    const language = req.query.language || "C#";
     const limit = +req.query.limit || 5;
 
-    let { data } = await axios.get(`${URL_BASE}/${username}/repos`);
+    const { data } = await axios.get(`${URL_BASE}/${username}/repos`);
 
-    if (!!language) {
-      return res.status(200).json(
-        data
-          .filter((item) => item.language === language)
-          .sort(compareDate)
-          .slice(0, limit)
-      );
-    }
-    return res.status(200).json(data.slice(0, limit));
+    return res.status(200).json(
+      data
+        .filter((item) => item.language === language)
+        .sort(compareDate)
+        .slice(0, limit)
+    );
   } catch (error) {
+    console.log(error);
     res
       .status(500)
       .json({ error: "Server with problem, enter in contact with support" });
