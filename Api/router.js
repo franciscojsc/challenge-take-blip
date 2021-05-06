a65c3d519992d5070e0a1adda672452f08cd66cc
Francisco Chaves <francisco.jschaves@gmail.com>
2021-05-06 19:36:42 -0300

    feat: Add controller to list repos

--- Api/router.js
+++ Api/router.js
@@ -1,36 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-const axios = require("axios");
-const URL_BASE = "https://api.github.com/orgs";
+const listRepos = require("./controllers/listRepos");
 
-router.get("/repo/:username", async (req, res) => {
-  try {
-    const { username } = req.params;
-
-    const language = req.query.language;
-    const limit = +req.query.limit || 5;
-
-    let { data } = await axios.get(`${URL_BASE}/${username}/repos`);
-
-    if (!!language) {
-      return res.status(200).json(
-        data
-          .filter((item) => item.language === language)
-          .sort(compareDate)
-          .slice(0, limit)
-      );
-    }
-    return res.status(200).json(data.slice(0, limit));
-  } catch (error) {
-    res
-      .status(500)
-      .json({ error: "Server with problem, enter in contact with support" });
-  }
-});
-
-function compareDate(a, b) {
-  return a.created_at < b.created_at;
-}
+router.get("/repo/:username", (req, res) => listRepos(req, res));
 
 module.exports = router;
