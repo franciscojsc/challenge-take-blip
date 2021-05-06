bb3c03c069d54a6e53ad55f7e329c147f0cc0579
Francisco Chaves <francisco.jschaves@gmail.com>
2021-05-06 19:28:59 -0300

    feat: Create file the router

--- Api/index.js
+++ Api/index.js
@@ -1,43 +1,14 @@
 const express = require("express");
-const axios = require("axios");
+const router = require("./router");
 const app = express();
 const port = process.env.PORT || 3000;
-const URL_BASE = "https://api.github.com/orgs";
 
 app.use(express.json());
+app.use(router);
 
 app.get("/", (req, res) => {
   res.json({ status: "OK" });
 });
-
-app.get("/repo/:username", async (req, res) => {
-  try {
-    const { username } = req.params;
-
-    const language = req.query.language;
-    const limit = +req.query.limit || 5;
-
-    let { data } = await axios.get(`${URL_BASE}/${username}/repos`);
-
-    if (!!language) {
-      return res.status(200).json(
-        data
-          .filter((item) => item.language === language)
-          .sort(compareDate)
-          .slice(0, limit)
-      );
-    }
-    return res.status(200).json(data.slice(0, limit));
-  } catch (error) {
-    res
-      .status(500)
-      .json({ error: "Server with problem, enter in contact with support" });
-  }
-});
-
-function compareDate(a, b) {
-  return a.created_at < b.created_at;
-}
 
 app.listen(port, () => {
   console.log(`Server listen in port ${port}`);
